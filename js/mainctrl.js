cd00180e9d9ad41fd3670902c278ec8178acc0c7
ccoburn <christopher.scott.coburn@gmail.com>
2017-11-13 14:32:27 -0700

    messing with timers

--- js/mainctrl.js
+++ js/mainctrl.js
@@ -6,8 +6,30 @@ $scope.getData = function() {
   service.getData().then(function(response){
     $scope.data = response;
     $scope.weatherImage = response.icon_url
+    var temp = parseInt(response.temp_f)
+    $scope.sprinkler = "Sprinklers are a go"
+    if ( temp < 38 || response.weather === 'Rain' || response.weather === "Light Rain") {
+      console.log('nope');
+        $scope.sprinkler = "No sprinklers today"
+    }
+    console.log("it ran");
   })
   }
   $scope.getData();
+
+$scope.sendZip = function(addZip) {
+  service.zip = addZip;
+  $scope.getData();
+ }
+
+ var now = new Date();
+$scope.millisTill15 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 30, 0, 0) - now;
+console.log($scope.millisTill15);
+if ($scope.millisTill15 < 0) {
+     $scope.millisTill15 += 86400000; // it's after 10am, try 10am tomorrow.
+}
+
+setTimeout(function(){ $scope.getData() }, $scope.millisTill15);
+
 
  })
--- js/service.js
+++ js/service.js
@@ -2,10 +2,10 @@ angular.module('sprinkler').service('service', function($http) {
 
 
  var id = '4d055b630b448d8f'
- var zip = '02123'
+ this.zip = '84043'
 
   this.getData = function() {
-    return $http.get('http://api.wunderground.com/api/' + id + '/conditions/q/' + zip + '.json').then(function(response)
+    return $http.get('http://api.wunderground.com/api/' + id + '/conditions/q/' + this.zip + '.json').then(function(response)
     {
       console.log(response.data.current_observation)
       return response.data.current_observation
