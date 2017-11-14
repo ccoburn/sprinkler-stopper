6d05c9cb9a9677b7ffc1f0ad5c3384ae2e04fcc2
ccoburn <christopher.scott.coburn@gmail.com>
2017-11-13 19:06:06 -0700

    Time functions correctly

--- js/mainctrl.js
+++ js/mainctrl.js
@@ -1,35 +1,89 @@
-angular.module('sprinkler').controller('mainCtrl', function($scope, service){
+angular.module('sprinkler').controller('mainCtrl', function($scope, service) {
 
-$scope.test= "hello, this app is working"
+  $scope.test = "hello, this app is working"
 
-$scope.getData = function() {
-  service.getData().then(function(response){
-    $scope.data = response;
-    $scope.weatherImage = response.icon_url
-    var temp = parseInt(response.temp_f)
-    $scope.sprinkler = "Sprinklers are a go"
-    if ( temp < 38 || response.weather === 'Rain' || response.weather === "Light Rain") {
-      console.log('nope');
+  $scope.getData = function() {
+    service.getData().then(function(response) {
+      $scope.data = response;
+      $scope.weatherImage = response.icon_url
+      var temp = parseInt(response.temp_f)
+      $scope.sprinkler = "Sprinklers are a go"
+      if (temp < 38 || response.weather === 'Rain' || response.weather === "Light Rain") {
+        console.log('nope');
         $scope.sprinkler = "No sprinklers today"
-    }
-    console.log("it ran");
-  })
+      }
+      console.log("it ran");
+    })
   }
   $scope.getData();
 
-$scope.sendZip = function(addZip) {
-  service.zip = addZip;
-  $scope.getData();
- }
+  $scope.sendZip = function(addZip) {
+    service.zip = addZip;
+    $scope.getData();
+  }
 
- var now = new Date();
-$scope.millisTill15 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 30, 0, 0) - now;
-console.log($scope.millisTill15);
-if ($scope.millisTill15 < 0) {
-     $scope.millisTill15 += 86400000; // it's after 10am, try 10am tomorrow.
+
+  $scope.sprinklerHour = 18;
+  $scope.sprinklerMinute = 55;
+  $scope.timer = function() {
+    var now = new Date();
+    $scope.millisTill = new Date(now.getFullYear(), now.getMonth(), now.getDate(), $scope.sprinklerHour, $scope.sprinklerMinute, 0, 0) - now;
+    if ($scope.millisTill < 0) {
+      $scope.millisTill += 86400000; // it's after 10am, try 10am tomorrow.
+    }
+
+    setTimeout(function() {
+      $scope.getData()
+    }, $scope.millisTill);
+
+    }
+    $scope.timer();
+
+$scope.timer2 = function() {
+  var now2 = new Date()
+  $scope.millisTill2 = new Date(now2.getFullYear(), now2.getMonth(), now2.getDate(), $scope.sprinklerHour, $scope.sprinklerMinute, 0, 0) - now2;
+  if ($scope.millisTill2 < 0) {
+    $scope.millisTill2 += 86400000; // it's after 10am, try 10am tomorrow.
+  }
+
+  //Get hours from milliseconds
+  var hours = $scope.millisTill2 / (1000*60*60);
+  var absoluteHours = Math.floor(hours);
+  var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+  //Get remainder from hours and convert to minutes
+  var minutes = (hours - absoluteHours) * 60;
+  var absoluteMinutes = Math.floor(minutes);
+  var m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
+
+  //Get remainder from minutes and convert to seconds
+  var seconds = (minutes - absoluteMinutes) * 60;
+  var absoluteSeconds = Math.floor(seconds);
+  var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+
+
+  $scope.timeLeft = h + ':' + m + ':' + s;
+
 }
 
-setTimeout(function(){ $scope.getData() }, $scope.millisTill15);
+$scope.changeTime = function(updateHour, updateMinute) {
+  $scope.sprinklerHour = updateHour;
+  $scope.sprinklerMinute = updateMinute;
+  $scope.timer();
+  $scope.timer2();
 
+}
 
- })
+setTimeout(function() {
+  $scope.getData()
+}, $scope.millisTill);
+
+  setInterval(function() {
+    $scope.$apply(function() {
+      $scope.timer2()
+    })
+  }, 1000)
+
+  //another setTimeout that changes the value in scope every second so we can see it on the screen and then set up the ability to change the target for both countdowns simultaniously
+
+})
